2d2c480b25d513efabc841a19975047ff0d0a19a
Bharath Kalyan S <sbharath35@gmail.com>
2024-03-11 10:04:29 +0530

    completed django tutorial (#1)

--- auth_service/api/serializers.py
+++ auth_service/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from api.models import Company
+from api.models import Company, Employee
 
 
 class CompanySerializers(serializers.HyperlinkedModelSerializer):
@@ -8,4 +8,12 @@ class CompanySerializers(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Company
+        fields = "__all__"
+
+
+class EmployeeSerializers(serializers.HyperlinkedModelSerializer):
+    employee_id = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Employee
         fields = "__all__"
--- auth_service/api/views.py
+++ auth_service/api/views.py
@@ -1,9 +1,31 @@
-from rest_framework import viewsets
+from django.http import JsonResponse
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from api.models import Company
-from api.serializers import CompanySerializers
+from api.models import Company, Employee
+from api.serializers import CompanySerializers, EmployeeSerializers
 
 
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializers
+
+    # companies/{companyId}/employees
+    @action(detail=True, methods=['get'])
+    def employees(self, request, pk=None):
+        try:
+            company = Company.objects.get(pk=pk)
+            employees = Employee.objects.filter(company=company)
+            employee_serializer = EmployeeSerializers(employees, many=True, context={'request': request})
+            return Response(employee_serializer.data)
+        except Company.DoesNotExist:
+            return JsonResponse({'message': 'Company Not Found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            print(e)
+            return JsonResponse({'message': 'Internal Server Error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializers
